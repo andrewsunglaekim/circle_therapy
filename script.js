c52776b4a39ca917c592e7771937ff4160b61aba
Andy <andrew.sunglae.kim@gmail.com>
2016-11-04 11:37:24 -0400

    moves files into models views folder removes scripts and logs

--- script.js
+++ script.js
@@ -1,56 +1,11 @@
 $(document).ready(function(){
-  console.log("hello");
-  $(document).on("click", function(evt){
-    createCenter(evt)
-    createRotator(evt, 100, -1, 0.33)
+  $(document).on("click",(evt) => {
+    let direction = -1
+    if (!($(evt.target).is("div") || $(evt.target).is("input"))){
+      let rotator = new Rotator(evt.pageX - 75, evt.pageY - 25, 50)
+      let rotatorView = new RotatorView(rotator, direction)
+      rotatorView.render()
+      direction *= -1
+    }
   })
-
-  function createCenter(evt){
-    var el = $("<div class='center'></div>")
-    el.css({
-      position: "absolute",
-      top: evt.pageY - 25 ,
-      left: evt.pageX -25
-    })
-    console.log(evt)
-    $("body").append(el)
-
-  }
-
-  function createRotator(evt, radius, direction, startPercent){
-    var el = $("<div class='rotator'></div>")
-    var originX = evt.pageX - 25
-    var originY = evt.pageY - 25
-    var t = convertPercentToT(startPercent)
-    var xValue = getXonCircumference(originX, radius, t)
-    var yValue = getYonCircumference(originY, radius, t)
-    el.css({
-      position: "absolute",
-      left: xValue,
-      top: yValue
-    })
-    $("body").append(el)
-    setInterval(function(){
-      t += direction * 0.02
-      xValue= getXonCircumference(originX, radius, t)
-      yValue= getYonCircumference(originY, radius, t)
-      el.css({
-        left: xValue,
-        top: yValue
-      })
-    }, 25)
-  }
 })
-
-// increment x or y over time, get opposite value through formula
-// adjust top and left properties based on x and y
-function getXonCircumference(a, r, t){
-  return a + r * Math.cos(t)
-}
-function getYonCircumference(b, r, t){
-  return b + r * Math.sin(t)
-}
-
-function convertPercentToT(percent){
-  return percent * Math.PI * 2
-}
